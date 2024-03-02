9955ded48cf4825527f3e066e968590f5b472b46
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-03-02 15:20:17 -0500

    Agregando un poco mas especifidad en el mensaje

--- mi_mensaje2.py
+++ mi_mensaje2.py
@@ -5,6 +5,8 @@ mi_frame = tkinter.Frame(raiz)
 mi_frame.pack()
 mensaje = "Adoro el Yogurt Congelado de fresas"
 etiqueta = tkinter.Label(mi_frame,text=mensaje)
+mensaje = "Adoro el Yogurt Congelado de fresas y cereal"
+etiqueta = tkinter.Label(mi_frame,text=mensaje)
 etiqueta.pack()
 
 raiz.mainloop()

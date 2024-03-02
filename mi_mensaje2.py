1866a842eaf092bdeacc849e13acba50f4b6cc67
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2024-03-02 15:16:38 -0500

    se ha cambiado el mensaje de la etiqueta de segundo archivo

---
+++ mi_mensaje2.py
@@ -0,0 +1,10 @@
+import tkinter
+
+raiz = tkinter.Tk()
+mi_frame = tkinter.Frame(raiz)
+mi_frame.pack()
+mensaje = "Adoro el Yogurt Congelado de fresas"
+etiqueta = tkinter.Label(mi_frame,text=mensaje)
+etiqueta.pack()
+
+raiz.mainloop()
